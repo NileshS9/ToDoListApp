3f199f233d98eb3fd82bff4fdfa8ab0d8c62dc89
NileshS9 <sawantnilesh891@gmail.com>
2025-06-23 21:32:34 +0530

    created a toDo file in model package

--- src/main/java/com/example/To_do_list/model/toDo.java
+++ src/main/java/com/example/To_do_list/model/toDo.java
@@ -1,4 +1,32 @@
 package com.example.To_do_list.model;
 
-public class toDo {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="todos")
+public class toDo{
+
+
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
